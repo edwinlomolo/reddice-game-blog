f19ef327ab7907348624701046b0698178a69472
3dw1nM0535 <workockmoses@gmail.com>
2018-01-06 00:28:06 +0300

    Render react main component

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
+// Server configuration
 import express from 'express';
 import path from 'path';
 
 const app = express();
+
+// Webpack bundler configuration
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev';
+
+// webpack middleware configuration
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));  
